94ed307da8a8ecec671bb318babd650d701403ad
Dominic Myers <annoyingmouse@gmail.com>
2020-04-28 15:22:16 +0000

    Changes to formatting

--- js/new_eyes.js
+++ js/new_eyes.js
@@ -11,62 +11,62 @@
           Y8b d88P                   
            "Y88P"                    
  */
-$(function(){
-    $("#YourFamilyDetails").on("click", ".eye-colour", function(){
-        var original = YourFamilyDetails.row($(this).parents('tr')).data();
-        $(".memberName").text(original.name);
-        $("#eyeModal").data("original", original);
-        if(original.hasOwnProperty("eyes") && original.eyes){
-            $("#eyeExists").prop("checked", true); 
-            $("#eyeColour").val(original["eye-colour"]).removeAttr("disabled");
-            $("#eyeModalPrimary").val("Update Eye Details")
-        }else{
-            $("#eyeModalPrimary").val("Add Eye Details")
+$(function () {
+  $("#YourFamilyDetails").on("click", ".eye-colour", function () {
+    var original = YourFamilyDetails.row($(this).parents('tr')).data();
+    $(".memberName").text(original.name);
+    $("#eyeModal").data("original", original);
+    if (original.hasOwnProperty("eyes") && original.eyes) {
+      $("#eyeExists").prop("checked", true);
+      $("#eyeColour").val(original["eye-colour"]).removeAttr("disabled");
+      $("#eyeModalPrimary").val("Update Eye Details")
+    } else {
+      $("#eyeModalPrimary").val("Add Eye Details")
+    }
+  });
+  $("#eyeExists").click(function () {
+    if ($(this).prop("checked")) {
+      $("#eyeColour").removeAttr("disabled");
+    } else {
+      $("#eyeColour").attr("disabled", "disabled").val("");
+    }
+  });
+  var eyeForm = $("#eyeForm").validate({
+    "rules": {
+      "eyeColour": {
+        "required": "#eyeExists:checked"
+      }
+    },
+    "submitHandler": function (form) {
+      var familyMember = $("#eyeModal").data("original");
+      $.each(data, function (k, v) {
+        if (
+          v.title === familyMember.title
+          &&
+          v.forename === familyMember.forename
+          &&
+          v.surname === familyMember.surname
+          &&
+          v.dob === familyMember.dob
+        ) {
+          if ($("#eyeExists").prop("checked")) {
+            v.eyes = true;
+            v["eye-colour"] = $("#eyeColour").val();
+          } else {
+            delete v.eyes;
+            delete v["eye-colour"];
+          }
         }
-    });
-    $("#eyeExists").click(function(){
-        if($(this).prop("checked")){
-            $("#eyeColour").removeAttr("disabled");
-        }else{
-            $("#eyeColour").attr("disabled", "disabled").val("");
-        }
-    });
-    var eyeForm = $("#eyeForm").validate({
-        "rules": {
-            "eyeColour": {
-                "required": "#eyeExists:checked"
-            }
-        },
-        "submitHandler": function(form) {
-            var familyMember = $("#eyeModal").data("original");
-            $.each(data, function(k, v){
-                if(
-                    v.title === familyMember.title
-                    &&
-                    v.forename === familyMember.forename
-                    &&
-                    v.surname === familyMember.surname
-                    &&
-                    v.dob === familyMember.dob
-                ){
-                    if($("#eyeExists").prop("checked")){
-                        v.eyes = true;
-                        v["eye-colour"] = $("#eyeColour").val();
-                    }else{
-                        delete v.eyes;
-                        delete v["eye-colour"];
-                    }
-                }
-            });
-            YourFamilyDetails.clear().rows.add(data).draw();
-            $("#eyeModal").modal("hide");
-        }
-    });
-    $("#eyeModal").on("hidden.bs.modal", function(){
-        eyeForm.resetForm(); 
-        document.getElementById("eyeForm").reset();
-        $("#eyeModal").data("original", "");
-        $(".memberName").text("");
-        $("#eyeColour").attr("disabled", "disabled");
-    });
+      });
+      YourFamilyDetails.clear().rows.add(data).draw();
+      $("#eyeModal").modal("hide");
+    }
+  });
+  $("#eyeModal").on("hidden.bs.modal", function () {
+    eyeForm.resetForm();
+    document.getElementById("eyeForm").reset();
+    $("#eyeModal").data("original", "");
+    $(".memberName").text("");
+    $("#eyeColour").attr("disabled", "disabled");
+  });
 });
--- js/new_hand.js
+++ js/new_hand.js
@@ -8,44 +8,44 @@
 888    888 888  888 888  888 Y88b 888 
 888    888 "Y888888 888  888  "Y88888 
  */
-$(function(){
-    $("#YourFamilyDetails").on("click", ".handedness", function(){
-        var original = YourFamilyDetails.row($(this).parents('tr')).data();
-        $(".memberName").text(original.name);
-        $("#handModal").data("original", original);
-        if(original.hasOwnProperty("hand")){
-            $("#handedness").val(original.hand);
-            $("#handModalPrimary").val("Update Handedness");
-        }else{
-            $("#handModalPrimary").val("Add Handedness")
+$(function () {
+  $("#YourFamilyDetails").on("click", ".handedness", function () {
+    var original = YourFamilyDetails.row($(this).parents('tr')).data();
+    $(".memberName").text(original.name);
+    $("#handModal").data("original", original);
+    if (original.hasOwnProperty("hand")) {
+      $("#handedness").val(original.hand);
+      $("#handModalPrimary").val("Update Handedness");
+    } else {
+      $("#handModalPrimary").val("Add Handedness")
+    }
+  });
+  $("#handModalPrimary").on("click", function (e) {
+    e.preventDefault();
+    var familyMember = $("#handModal").data("original");
+    $.each(data, function (k, v) {
+      if (
+        v.title === familyMember.title
+        &&
+        v.forename === familyMember.forename
+        &&
+        v.surname === familyMember.surname
+        &&
+        v.dob === familyMember.dob
+      ) {
+        if (~~$("#handedness").val().length) {
+          v.hand = $("#handedness").val();
+        } else {
+          delete v.hand;
         }
+      }
     });
-    $("#handModalPrimary").on("click", function(e){
-        e.preventDefault(); 
-        var familyMember = $("#handModal").data("original");
-        $.each(data, function(k, v){
-            if(
-                v.title === familyMember.title
-                &&
-                v.forename === familyMember.forename
-                &&
-                v.surname === familyMember.surname
-                &&
-                v.dob === familyMember.dob
-            ){
-                if(~~$("#handedness").val().length){
-                    v.hand = $("#handedness").val();
-                }else{
-                    delete v.hand;
-                }
-            }
-        });
-        YourFamilyDetails.clear().rows.add(data).draw();
-        $("#handModal").modal("hide");
-        $("#handedness").val("");
-    });
-    $("#handModal").on("hidden.bs.modal", function(){
-        $("#handModal").data("original", "");
-        $(".memberName").text("");
-    });
+    YourFamilyDetails.clear().rows.add(data).draw();
+    $("#handModal").modal("hide");
+    $("#handedness").val("");
+  });
+  $("#handModal").on("hidden.bs.modal", function () {
+    $("#handModal").data("original", "");
+    $(".memberName").text("");
+  });
 });
