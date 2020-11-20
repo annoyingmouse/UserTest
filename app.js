3015f8c440e5ca1c4cf0269d2b95f0f119b07fd3
Dominic Myers <annoyingmouse@gmail.com>
2020-11-20 15:56:18 +0000

    SPA

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*
- * To configure for IE11
- * npm install -g rollup
- * rollup app.js --file bundle.js --format iife
- * paste bundle.js into the left-hand panel
- * babeljs.io/repl#?browsers=defaults
- * Copy over existing bundle.js with contents
- * of the right-hand panel.
- * Job done.
- */
-
 import { store } from './store/store.js';
 import { router } from './router/router.js';
 
